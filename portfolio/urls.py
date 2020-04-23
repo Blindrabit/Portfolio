156eccbb077b06d38f57d7d9f8def49e0915ec2c
Adam Tucker <a.r.tucker@hotmail.co.uk>
2020-04-23 17:22:36 +0100

    index page fix

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from project import views as project_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('project/', include('project.urls')),
+    path('', project_views.index, name= 'index'),
 ]
